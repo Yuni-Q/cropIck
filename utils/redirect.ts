08dc7a3768fca124d583cd442bf6370c1095a837
Yuni-Q <lyh6425@gmail.com>
2020-11-27 10:52:54 +0900

    feat: 유틸 함수 추가

---
+++ utils/redirect.ts
@@ -0,0 +1,27 @@
+import { ServerResponse } from "http";
+
+export default (): string => {
+    return ''
+};
+
+export const redirectRoot = (res?: ServerResponse): void => {
+    const path = '/'
+    if(res) {
+        res.setHeader('location', path);
+        res.statusCode = 302;
+        res.end();
+    } else {
+        window.location.pathname = path
+    }
+}
+
+export const redirectLogin = (res?: ServerResponse): void => {
+    const path = '/login'
+    if(res) {
+        res.setHeader('location', path);
+        res.statusCode = 302;
+        res.end();
+    } else {
+        window.location.pathname = path;
+    }
+}
