a2861c116eb0ab7f26235c38511fd3f9424dd4cf
Yuni-Q <lyh6425@gmail.com>
2020-11-30 23:21:33 +0900

    first build

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,18 +9,13 @@ import News from '../components/News';
 import PopularCommunity from '../components/PopularCommunity';
 import PopularCommunityContent from '../components/PopularCommunityContent';
 import Rank from '../components/Rank';
-import { PageContext } from './_app';
-
-interface Props {
-	data: number;
-}
 
 export enum Category {
 	PLACE,
 	CROPS,
 }
 
-const Main: NextPage<Props> = ({ data }) => {
+const Main: NextPage = () => {
 	
 	return (
 		<StyledWrapper>
@@ -40,22 +35,6 @@ const Main: NextPage<Props> = ({ data }) => {
 			<Footer />
 		</StyledWrapper>
 	);
-};
-
-
-interface ServerSideProps {
-	props: {
-		data: number;
-	}
-}
-
-export const getServerSideProps = async ({req, res}: PageContext): Promise<ServerSideProps | void> => {
-	console.log(req, res);
-	return {
-		props: {
-			data: 123,
-		}
-	}
 };
 
 export default Main;
